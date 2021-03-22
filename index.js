898dadace280fae6e1497513906868049c80f555
IlyusinAV <andreyilusin@mail.ru>
2021-03-22 20:56:40 +0300

    1 file modified

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 // Import stylesheets
-import './style.css';
+import "./style.css";
 
 // Write Javascript code!
-const appDiv = document.getElementById('app');
+const appDiv = document.getElementById("app");
 appDiv.innerHTML = `<h1>Currency rates</h1>`;
+const dateNow = new Date();
+const dateButton = document.createElement("button");
+dateButton.setAttribute("class", "app__button");
+const buttonName =
+  dateNow.getFullYear() + "-" + dateNow.getMonth() + "-" + dateNow.getDate();
+dateButton.innerHTML = buttonName;
+appDiv.appendChild(dateButton);
