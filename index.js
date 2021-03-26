98a20b906f9a6a91418a9fd4cf86ad63d60acbce
IlyusinAV <andreyilusin@mail.ru>
2021-03-26 18:18:01 +0300

    1 file modified

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 // Import stylesheets
 import "./style.css";
 
-// Write Javascript code!
 const API_URL = "https://api.exchangeratesapi.io";
 const BASE_CURR = "RUB";
+
+const adjustNumber2 = n => (n + 100).toString().slice(-2);
+const dateToString = date =>
+  `${date.getFullYear()}-${adjustNumber2(date.getMonth() + 1)}-${adjustNumber2(
+    date.getDate()
+  )}`;
 
 const dateNow = new Date();
 const dateArray = new Array(7).fill(dateNow);
@@ -16,14 +21,47 @@ for (let i = 1; i < 7; i++) {
 const cr_model = {
   rate_date: "",
   rate_base: "",
-  rates: [{ rate: "", value: "" }]
+  rates: [{ code: "", value: "" }]
 };
 
-const adjustNumber2 = n => (n + 100).toString().slice(-2);
-const dateToString = date =>
-  `${date.getFullYear()}-${adjustNumber2(date.getMonth() + 1)}-${adjustNumber2(
-    date.getDate()
-  )}`;
+const cr_view = ({ rate_date, rate_base, rates }) => {
+  const contentHTML = `
+    Base: ${rate_base}<br />
+    Date: ${rate_date}<br />
+    <table>
+    <thead>
+      <tr>
+        <th>Code</th>
+        <th>Value</th>
+      </tr>
+    </thead>
+    <tbody>
+      ${rates
+        .map(
+          rate => `
+            <tr>
+              <td>${rate.code}</td>
+              <td>${rate.value}</td>
+            </tr>
+          `
+        )
+        .join("")}
+    </tbody>
+    </table>
+  `;
+};
+
+const cr_controls = dateArray => {
+  const controlsHTML = `
+  ${dateArray
+    .map(
+      date => `
+      <button>${dateToString(date)}</button>
+      `
+    )
+    .join("")}
+  `;
+};
 
 const appDiv = document.getElementById("app");
 appDiv.innerHTML = `<h1>Currency rates</h1>`;
